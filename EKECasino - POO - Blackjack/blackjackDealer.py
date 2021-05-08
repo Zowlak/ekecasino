b32618280d94c440747bca41f5ee4975cbfb309e
10K <bismuthdev@gmail.com>
2021-05-08 12:48:41 +0200

    Will put the project into a venv

--- EKECasino - POO - Blackjack/blackjackDealer.py
+++
@@ -1,7 +0,0 @@
-class Dealer:
-    """Ceci est la classe du croupier. Toutes ses actions sont dans la classe Table."""
-    def __init__(self):
-        """Encore des variables"""
-        self.name = "Dealer"
-        self.score = 0
-        self.hand = []

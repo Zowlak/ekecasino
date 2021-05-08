9bd7c6243e0f9434d54d164cb93e5e3dc071c3b7
10K <bismuthdev@gmail.com>
2021-05-08 13:24:24 +0200

    Now it should be ok

---
+++ EKECasino-POO-Blackjack/blackjackDealer.py
@@ -0,0 +1,7 @@
+class Dealer:
+    """Ceci est la classe du croupier. Toutes ses actions sont dans la classe Table."""
+    def __init__(self):
+        """Encore des variables"""
+        self.name = "Dealer"
+        self.score = 0
+        self.hand = []
---
+++ EKECasino-POO-Blackjack/blackjackPlayer.py
@@ -0,0 +1,37 @@
+import blackjackDealer as Bjd
+from colorama import Fore, Style, Back
+
+
+class Player(Bjd.Dealer):
+    """Ceci est la classe du joueur. Ses actions sont listés en dessous."""
+    def __init__(self, name, funds, bet=0):
+        """Définition des variables"""
+        super().__init__()
+        self.name = name
+        self.funds = funds
+        self.bet = bet
+
+    def place_bet(self, amount):
+        """Appelé à chaque tour, il montre combien on a dans notre banque et notre mise"""
+        self.funds -= amount
+        self.bet += amount
+
+    def payout(self):
+        """Le joueur gagne l'équivalent de sa mise en plus de reprendre sa mise initiale"""
+        self.funds += (self.bet * 2)
+        self.bet = 0
+
+    @staticmethod  # Permet d'appeler la fonction n'importe quand.
+    def hit_or_stick():
+        """La fonction qui demande le joueur s'il veut tirer une nouvelle carte ou pas. A vous de voir"""
+        while True:
+            choice = input("Voulez-vous une autre carte (O/N)? ")
+            if choice.lower().startswith('o'):
+                print(chr(27) + "[2J")
+                return True
+            elif choice.lower().startswith('n'):
+                print(chr(27) + "[2J")
+                return False
+            else:
+                print(Back.RED + Fore.BLACK + "Je n'ai pas compris, veuillez répéter pour le sourd s'il vous plaît")
+                continue
